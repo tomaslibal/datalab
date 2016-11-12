445750aabb4a2d3a58bf2a376f9aeeb1f79edc40
Tomas Libal <tomas@libal.eu>
2016-11-12 16:41:19 +0800

    add edit_labels path and specify the home path such that it is less greedy; add the edit_labels view

--- lab/urls.py
+++ lab/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'', views.home, name='home'),
+    url(r'^$', views.home, name='home'),
+    url(r'edit_labels/(?P<datapoint_id>\d{0,16})/$', views.edit_labels, name='edit_labels'),
 ]
--- lab/views.py
+++ lab/views.py
@@ -6,3 +6,7 @@ def home(request):
     datapoints = Datapoint.objects.all()
     return render(request, 'lab/home.html', {'datapoints': datapoints})
 
+def edit_labels(request, datapoint_id):
+    datapoint = Datapoint.objects.filter(id=datapoint_id)
+    return render(request, 'lab/edit_labels.html', {'datapoint': datapoint})
+
