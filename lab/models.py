56b044a37403c070fc3561b83089cfaf4d4acdae
Tomas Libal <tomas@libal.eu>
2016-11-14 21:39:01 +0800

    add __str__ magic methods and add many-to-many rel. on datapoint to label

--- lab/models.py
+++ lab/models.py
@@ -9,13 +9,22 @@ class Entity(models.Model):
     def __str__(self):
         return self.name
 
+class Label(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.CharField(blank=True, max_length=128)
+    weight = models.FloatField(default=1.0)
+
+    def __str__(self):
+        return self.name
+
 class Datapoint(models.Model):
     entity_type = models.ForeignKey(Entity, on_delete=models.CASCADE, default='')
     data = models.TextField()
     description = models.CharField(blank=True, max_length=256)
     active = models.BooleanField(default=True)
+    labels = models.ManyToManyField(Label)
 
-class Label(models.Model):
-    name = models.CharField(max_length=128)
-    description = models.CharField(blank=True, max_length=128)
-    weight = models.FloatField(default=1.0)
+    def __str__(self):
+        return self.description
+
+
