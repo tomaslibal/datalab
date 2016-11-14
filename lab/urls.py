80229b4777bc1166386cd8c8586d93683b15a01d
Tomas Libal <tomas@libal.eu>
2016-11-14 22:00:56 +0800

    add handlers for add_datapoint and as_image urls

--- lab/urls.py
+++ lab/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from . import views
+from .AddDatapointView import AddDatapointView
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'edit_labels/(?P<datapoint_id>\d{0,16})/$', views.edit_labels, name='edit_labels'),
+    url(r'add_datapoint/$', AddDatapointView.as_view(), name='add_datapoint'),
+    url(r'^api/datapoint/as_image/(?P<datapoint_id>\d{0,16})/$', views.entity_image, name='entity_image'),
 ]
