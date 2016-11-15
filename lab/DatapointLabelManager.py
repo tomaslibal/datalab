96662bff8d6ee23ab71c19e772f2f32608dd9918
Tomas Libal <tomas@libal.eu>
2016-11-16 00:40:06 +0800

    add a controller for adding a label to a datapoint from the api url

---
+++ lab/DatapointLabelManager.py
@@ -0,0 +1,17 @@
+from django.views.generic import View
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
+
+from .models import Datapoint, Label
+
+class DatapointLabelManager(View):
+    def get(self, request):
+        return HttpResponseRedirect('/lab/home.html')
+    def post(self, request, datapoint_id):
+        label_name = request.POST.get('name', None)
+        if label_name is not None:
+            obj, created = Label.objects.get_or_create(name=label_name)
+            dp = Datapoint.objects.get(id=datapoint_id)
+            dp.labels.add(obj)
+            dp.save()
+            return HttpResponse('OK')
+        return HttpResponseNotFound('Error: datapoint id not found')
