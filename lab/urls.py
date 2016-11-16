cabe34a6fac3a8d1e7a20d56d64a7301cdc345b2
Tomas Libal <tomas@libal.eu>
2016-11-16 21:41:44 +0800

    preprend api to the url for getting the number of datapoints using the given label
    because the url returns just data

--- lab/urls.py
+++ lab/urls.py
@@ -7,11 +7,13 @@ from .DatapointLabelManager import DatapointLabelManager
 urlpatterns = [
     url(r'^$', views.home, name='home'),
 
-    url(r'edit_labels/(?P<datapoint_id>\d{0,16})/$', views.edit_labels, name='edit_labels'),
+    url(r'labels/$', views.labels, name='labels'),
+    url(r'edit_labels/(?P<datapoint_id>\d{0,16})/$', views.edit_labels, name='edit_labels'), # change this url to /datapoints/#id/labels
     url(r'add_datapoint/$', AddDatapointView.as_view(), name='add_datapoint'),
     url(r'entities/$', views.entities, name='entities'),
     url(r'entities/(?P<entity_id>\d{0,16})/$', views.entity_detail, name='entity_detail'),
 
+    url(r'api/labels/(?P<label_id>\d{0,16})/usagenum/$', views.num_datapoints_use_label, name='num_datapoints_use_label'),
     url(r'^api/datapoints/as_csv/$', CsvDownloader.as_view(), name='download_as_csv'),
     url(r'^api/datapoint/(?P<datapoint_id>\d{0,16})/labels/$', DatapointLabelManager.as_view(), name='datapoint_label_manager'),
     url(r'^api/datapoint/as_image/(?P<datapoint_id>\d{0,16})/$', views.entity_image, name='entity_image'),
