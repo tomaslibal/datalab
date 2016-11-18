86e09839a34830f142ef8d6694cc7ba2c6aabf50
Tomas Libal <tomas@libal.eu>
2016-11-18 22:09:38 +0800

    make labels/ page url matcher less greedy
    this fixes the problem when the "add label" quick form did get routed to the wront controller and thus did not save the new label

--- lab/urls.py
+++ lab/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
 
     url(r'settings/$', views.settings, name='settings'),
 
-    url(r'labels/$', views.labels, name='labels'),
+    url(r'^labels/$', views.labels, name='labels'),
     url(r'edit_labels/(?P<datapoint_id>\d{0,16})/$', views.edit_labels, name='edit_labels'), # change this url to /datapoints/#id/labels
     url(r'add_datapoint/$', AddDatapointView.as_view(), name='add_datapoint'),
     url(r'entities/$', views.entities, name='entities'),
