db1e88a44480326128647e73778a361b6f837bc7
Tomas Libal <tomas@libal.eu>
2016-11-21 23:58:01 +0800

    actually use a working code instead of the demo code

--- tests/lab/test_views.py
+++ tests/lab/test_views.py
@@ -1,29 +1,28 @@
 from django.contrib.auth.models import AnonymousUser, User
 from django.test import TestCase, RequestFactory
 
-from .views import MyView, my_view
+from lab.views import home
 
-class SimpleTest(TestCase):
+class ViewsTest(TestCase):
     def setUp(self):
-        # Every test needs access to the request factory.
         self.factory = RequestFactory()
         self.user = User.objects.create_user(
-            username='jacob', email='jacob@…', password='top_secret')
+            username='foo', email='foo@example.com', password='bar')
 
-    def test_details(self):
+    def test_home(self):
         # Create an instance of a GET request.
-        request = self.factory.get('/customer/details')
+        request = self.factory.get('/')
 
         # Recall that middleware are not supported. You can simulate a
         # logged-in user by setting request.user manually.
-        request.user = self.user
+        # request.user = self.user
 
         # Or you can simulate an anonymous user by setting request.user to
         # an AnonymousUser instance.
         request.user = AnonymousUser()
 
         # Test my_view() as if it were deployed at /customer/details
-        response = my_view(request)
+        response = home(request)
         # Use this syntax for class-based views.
-        response = MyView.as_view()(request)
+        # response = MyView.as_view()(request)
         self.assertEqual(response.status_code, 200)
