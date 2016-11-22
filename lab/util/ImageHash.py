5facecb16b661be4321b3d2751efaf4d972954f3
Tomas Libal <tomas@libal.eu>
2016-11-22 22:33:01 +0800

    basic hashing as an MVP

--- lab/util/ImageHash.py
+++ lab/util/ImageHash.py
@@ -1,3 +1,30 @@
+import numpy as np
+import hashlib
+import math
+
 class ImageHash():
-    def hash(self):
-        return ''
+    def hash(self, image_pixels):
+        cells = 16
+        w = len(image_pixels) # rows
+        h = len(image_pixels[0]) # columns
+        w_step = math.ceil(w / cells)
+        h_step = math.ceil(h / cells)
+        means = [[0 for i in range(cells)] for j in range(cells)]
+        for i in range(0, w):
+            for j in range(0, h):
+                cell_i = math.floor(i / w_step)
+                cell_j = math.floor(j / h_step)
+                means[cell_i][cell_j] += float(image_pixels[i][j])
+
+        means = np.array(means)
+        num_pix_in_cell = w_step * h_step
+
+        means //= float(num_pix_in_cell)
+
+        means = means.flatten('F')
+
+        h = hash(frozenset(means.tolist()))
+
+        md5 = hashlib.md5((str(h).encode('utf-8')))
+
+        return md5.hexdigest()
--- tests/lab/testImageHash.py
+++ tests/lab/testImageHash.py
@@ -8,5 +8,14 @@ class SimpleTest(TestCase):
         pass
 
     def test_hash_returns_string(self):
-        hash = self.hasher.hash()
+        hash = self.hasher.hash([[0, 0, 0, 0],[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
         self.assertEqual(True, isinstance(hash, str))
+
+    def test_hash_returns_non_empty_string(self):
+        hash = self.hasher.hash([[0, 0, 0, 0],[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
+        self.assertEqual(True, len(hash) > 0)
+
+    def test_two_different_inputs_return_different_hashes(self):
+        hash = self.hasher.hash([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
+        hash2 = self.hasher.hash([[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]])
+        self.assertNotEqual(hash, hash2)
