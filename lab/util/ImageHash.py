7d3b72939c8a98a554f1871e9c60e2c5b5e91500
Tomas Libal <tomas@libal.eu>
2016-12-04 13:19:01 +0800

    add median method and misc. changes
    - see the git diff

--- lab/util/ImageHash.py
+++ lab/util/ImageHash.py
@@ -1,21 +1,30 @@
 import numpy as np
-import hashlib
 import math
 
 class ImageHash():
+    def median(self, arr):
+        return np.median(arr)
+
+    def print_as_ascii(self, bits, cols, rows):
+        return np.array(bits).reshape(cols, rows)
+
     def to_bits(self, means, image_pixels, cells):
         w = len(image_pixels)  # rows
         h = len(image_pixels[0])  # columns
+
         w_step = math.ceil(w / cells)
         h_step = math.ceil(h / cells)
+
         HALF = (w_step * h_step) / 2
+
         num_above = [[0 for i in range(cells)] for j in range(cells)]
         bits = [[0 for i in range(cells)] for j in range(cells)]
+
         for i in range(0, w):
             for j in range(0, h):
                 cell_i = math.floor(i / w_step)
                 cell_j = math.floor(j / h_step)
-                if image_pixels[i][j] > int(means[cell_i][cell_j]):
+                if int(image_pixels[i][j]) > int(means[cell_i][cell_j]):
                     num_above[cell_i][cell_j] += 1
 
         idx = 0
@@ -32,13 +41,15 @@ class ImageHash():
     def to_hex(self, bits_array):
         return '{0:0={b}x}'.format(int(''.join([str(x) for x in bits_array]), 2), b=len(bits_array) // 4)
 
-    def hash(self, image_pixels):
-        cells = 16
+    def hash(self, image_pixels, cells = 16):
         w = len(image_pixels) # rows
         h = len(image_pixels[0]) # columns
+
         w_step = math.ceil(w / cells)
         h_step = math.ceil(h / cells)
+
         means = [[0 for i in range(cells)] for j in range(cells)]
+
         for i in range(0, w):
             for j in range(0, h):
                 cell_i = math.floor(i / w_step)
@@ -49,8 +60,6 @@ class ImageHash():
         num_pix_in_cell = w_step * h_step
 
         means //= num_pix_in_cell
-
-        # means = means.flatten()
 
         bits = self.to_bits(means, image_pixels, cells)
 
