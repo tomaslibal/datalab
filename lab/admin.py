70dbf188dd619c147415bdd672b6de8f7ea793c3
Tomas Libal <tomas@libal.eu>
2016-12-04 15:01:52 +0800

    add Dataset to the admin registered models

--- lab/admin.py
+++ lab/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import UserDefinedEntity, Datapoint, Label
+from .models import UserDefinedEntity, Datapoint, Label, Dataset
 
 # Register your models here.
 admin.site.register(UserDefinedEntity)
 admin.site.register(Datapoint)
 admin.site.register(Label)
+admin.site.register(Dataset)
 
