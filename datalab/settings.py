c227c01aac2044385d2bcb918bcd1f7f106e4822
Tomas Libal <tomas@libal.eu>
2017-01-07 09:33:52 +0800

    add py2neo and neomodel

--- datalab/settings.py
+++ datalab/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+from neomodel import config
+config.DATABASE_URL = 'bolt://neo4j:neo4j@localhost:7687'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
